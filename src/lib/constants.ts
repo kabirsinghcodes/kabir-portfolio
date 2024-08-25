72b1abe1eefa9f022b59c445141cd0a7e93c3e50
Kabir Singh <kabirsingh0405@gmail.com>
2024-08-25 22:51:02 +0100

    topmate link added

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -14,7 +14,7 @@ export const LINKS = {
   instagram: 'https://www.instagram.com/kabirosky/',
   youtube: 'https://www.youtube.com/@Kabirosky',
   discord: 'https://discordapp.com/users/163300027618295808',
-  cal:'https://cal.com/kabirosky',
+  cal:'https://topmate.io/kabirsingh/',
 }
 
 // Global
